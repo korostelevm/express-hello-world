28a2f84de3f7fc0e40a268710e1b619c9ac4a4f6
Mike <korostelevm@gmail.com>
2021-07-20 06:38:26 -0400

    asdf

--- app.js
+++ app.js
@@ -2,13 +2,38 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
  
+const AWS = require("aws-sdk");
+AWS.config.update({
+  region: process.env.region || "us-east-2",
+});
+
+const publish = async function (msg) {
+   var params = {
+     Message: JSON.stringify(msg),
+     TopicArn: 'arn:aws:sns:us-east-2:348655018330:cyclic-per-region-init-AppLogsTopic-179X4JB0VLI5T',
+     MessageAttributes: {
+       app_id: {
+         DataType: "String",
+         StringValue: 'korostelevm-helloworld',
+       },
+     },
+   };
+   console.log(params)
+   await sns.publish(params).promise();
+ };
+ 
+ 
+
+
 const VERSION_FILENAME='./.git/refs/heads/main'
 
 app.get('/', (req, res) => {
   console.log('[hello-world] root handler called')
+  var r = await publish({ts:Date.now(), line:'sadfasfd'});
+
   res
     .set('x-powered-by', 'cyclic.sh')
-    .send('<h1>Hello Worlhhhhhhd!</h1>')
+    .send(JSON.stringify(r))
     .end()
 })
 

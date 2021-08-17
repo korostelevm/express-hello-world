37ea789ae6beb4c57d091893fda073392bd304da
Mike <korostelevm@gmail.com>
2021-08-17 10:03:18 -0400

    asdf

--- app.js
+++ app.js
@@ -19,144 +19,142 @@ var jira = new JiraApi({
 });
 
 app.post('/git', async (req,res) => {
-
+var result = {a:'none'}
   console.log(req.body)
   try{
+    result = req.body
     //  console.log(process.env)
     //  console.log(jira)
     // jira.updateIssue('10000',{
   
     // })
   
-   var r = await jira.addNewIssue({
-    "update": {},
-    "fields": {
-      "summary": req.body.issue.title,
-      "description": req.body.issue.body,
-      // "parent": {
-      //   "key": "HELLO"
-      // },
-      "issuetype": {
-        "id": "10001"
-      },
-    //   "components": [
-    //     {
-    //       "id": "10000"
-    //     }
-    //   ],
-    //   "customfield_20000": "06/Jul/19 3:25 PM",
-    //   "customfield_40000": {
-    //     "type": "doc",
-    //     "version": 1,
-    //     "content": [
-    //       {
-    //         "type": "paragraph",
-    //         "content": [
-    //           {
-    //             "text": "Occurs on all orders",
-    //             "type": "text"
-    //           }
-    //         ]
-    //       }
-    //     ]
-    //   },
-    //   "customfield_70000": [
-    //     "jira-administrators",
-    //     "jira-software-users"
-    //   ],
-      "project": {
-        "key": "HELLO"
-      },
-    //   "description": {
-    //     "type": "doc",
-    //     "version": 1,
-    //     "content": [
-    //       {
-    //         "type": "paragraph",
-    //         "content": [
-    //           {
-    //             "text": "Order entry fails when selecting supplier.",
-    //             "type": "text"
-    //           }
-    //         ]
-    //       }
-    //     ]
-    //   },
-    //   "reporter": {
-    //     "id": "5b10a2844c20165700ede21g"
-    //   },
-    //   "fixVersions": [
-    //     {
-    //       "id": "10001"
-    //     }
-    //   ],
-    //   "customfield_10000": "09/Jun/19",
-    //   "priority": {
-    //     "id": "20000"
-    //   },
-    //   "labels": [
-    //     "bugfix",
-    //     "blitz_test"
-    //   ],
-    //   "timetracking": {
-    //     "remainingEstimate": "5",
-    //     "originalEstimate": "10"
-    //   },
-    //   "customfield_30000": [
-    //     "10000",
-    //     "10002"
-    //   ],
-    //   "customfield_80000": {
-    //     "value": "red"
-    //   },
-    //   "security": {
-    //     "id": "10000"
-    //   },
-    //   "environment": {
-    //     "type": "doc",
-    //     "version": 1,
-    //     "content": [
-    //       {
-    //         "type": "paragraph",
-    //         "content": [
-    //           {
-    //             "text": "UAT",
-    //             "type": "text"
-    //           }
-    //         ]
-    //       }
-    //     ]
-    //   },
-    //   "versions": [
-    //     {
-    //       "id": "10000"
-    //     }
-    //   ],
-    //   "duedate": "2019-05-11",
-    //   "customfield_60000": "jira-software-users",
-    //   "customfield_50000": {
-    //     "type": "doc",
-    //     "version": 1,
-    //     "content": [
-    //       {
-    //         "type": "paragraph",
-    //         "content": [
-    //           {
-    //             "text": "Could impact day-to-day work.",
-    //             "type": "text"
-    //           }
-    //         ]
-    //       }
-    //     ]
-    //   },
-    //   "assignee": {
-    //     "id": "5b109f2e9729b51b54dc274d"
-    //   }
-    }
-  })
-  // console.log(req.query)
-    //  var sprints = await jira.searchJira(`Summary ~ ${req.query.issue}`)
-    console.log(r)
+  //  var r = await jira.addNewIssue({
+  //   "update": {},
+  //   "fields": {
+  //     "summary": req.body.issue.title,
+  //     "description": req.body.issue.body,
+  //     // "parent": {
+  //     //   "key": "HELLO"
+  //     // },
+  //     "issuetype": {
+  //       "id": "10001"
+  //     },
+  //   //   "components": [
+  //   //     {
+  //   //       "id": "10000"
+  //   //     }
+  //   //   ],
+  //   //   "customfield_20000": "06/Jul/19 3:25 PM",
+  //   //   "customfield_40000": {
+  //   //     "type": "doc",
+  //   //     "version": 1,
+  //   //     "content": [
+  //   //       {
+  //   //         "type": "paragraph",
+  //   //         "content": [
+  //   //           {
+  //   //             "text": "Occurs on all orders",
+  //   //             "type": "text"
+  //   //           }
+  //   //         ]
+  //   //       }
+  //   //     ]
+  //   //   },
+  //   //   "customfield_70000": [
+  //   //     "jira-administrators",
+  //   //     "jira-software-users"
+  //   //   ],
+  //     "project": {
+  //       "key": "HELLO"
+  //     },
+  //   //   "description": {
+  //   //     "type": "doc",
+  //   //     "version": 1,
+  //   //     "content": [
+  //   //       {
+  //   //         "type": "paragraph",
+  //   //         "content": [
+  //   //           {
+  //   //             "text": "Order entry fails when selecting supplier.",
+  //   //             "type": "text"
+  //   //           }
+  //   //         ]
+  //   //       }
+  //   //     ]
+  //   //   },
+  //   //   "reporter": {
+  //   //     "id": "5b10a2844c20165700ede21g"
+  //   //   },
+  //   //   "fixVersions": [
+  //   //     {
+  //   //       "id": "10001"
+  //   //     }
+  //   //   ],
+  //   //   "customfield_10000": "09/Jun/19",
+  //   //   "priority": {
+  //   //     "id": "20000"
+  //   //   },
+  //   //   "labels": [
+  //   //     "bugfix",
+  //   //     "blitz_test"
+  //   //   ],
+  //   //   "timetracking": {
+  //   //     "remainingEstimate": "5",
+  //   //     "originalEstimate": "10"
+  //   //   },
+  //   //   "customfield_30000": [
+  //   //     "10000",
+  //   //     "10002"
+  //   //   ],
+  //   //   "customfield_80000": {
+  //   //     "value": "red"
+  //   //   },
+  //   //   "security": {
+  //   //     "id": "10000"
+  //   //   },
+  //   //   "environment": {
+  //   //     "type": "doc",
+  //   //     "version": 1,
+  //   //     "content": [
+  //   //       {
+  //   //         "type": "paragraph",
+  //   //         "content": [
+  //   //           {
+  //   //             "text": "UAT",
+  //   //             "type": "text"
+  //   //           }
+  //   //         ]
+  //   //       }
+  //   //     ]
+  //   //   },
+  //   //   "versions": [
+  //   //     {
+  //   //       "id": "10000"
+  //   //     }
+  //   //   ],
+  //   //   "duedate": "2019-05-11",
+  //   //   "customfield_60000": "jira-software-users",
+  //   //   "customfield_50000": {
+  //   //     "type": "doc",
+  //   //     "version": 1,
+  //   //     "content": [
+  //   //       {
+  //   //         "type": "paragraph",
+  //   //         "content": [
+  //   //           {
+  //   //             "text": "Could impact day-to-day work.",
+  //   //             "type": "text"
+  //   //           }
+  //   //         ]
+  //   //       }
+  //   //     ]
+  //   //   },
+  //   //   "assignee": {
+  //   //     "id": "5b109f2e9729b51b54dc274d"
+  //   //   }
+  //   }
+  // })
   
   }catch(e){
     // console.log()
@@ -170,7 +168,7 @@ app.post('/git', async (req,res) => {
   
   return res
   .set('x-powered-by', 'cyclic.sh')
-  .json({})
+  .json(result)
   .end()
 })
 

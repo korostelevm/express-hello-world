19ace94b6386e8576b3be701030578dc53493d20
forstmeier <john.forstmeier@gmail.com>
2021-08-24 22:10:50 -0400

    Add more logging

--- app.js
+++ app.js
@@ -2,37 +2,42 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 
-const VERSION_FILENAME='./.git/refs/heads/main'
+const VERSION_FILENAME = './.git/refs/heads/main'
 
 app.get('/', (req, res) => {
-  console.log('[hello-world] root handler called')
-  res
-    .set('x-powered-by', 'cyclic.sh')
-    .send('<h1>Hello World!</h1>')
-    .end()
+	console.log('[hello-world] root handler called')
+
+	console.log('req:', req);
+	console.log('res:', res);
+	console.log('env:', process.env);
+
+	res
+		.set('x-powered-by', 'cyclic.sh')
+		.send('<h1>Hello World!</h1>')
+		.end()
 })
 
-app.use('*', (req,res) => {
-  console.log('[hello-world] Star handler called')
-  let version = 'unknown'
-  if (fs.existsSync(VERSION_FILENAME)) {
-    version = fs.readFileSync(VERSION_FILENAME).toString().substr(0,8)
-  }
-  res
-    .set('x-powered-by', 'cyclic.sh')
-    .json({
-      msg: "Not strickly part of the hello world but you get the picture.",
-      version,
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      path: req.params[0],
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-    })
-    .end()
+app.use('*', (req, res) => {
+	console.log('[hello-world] Star handler called')
+	let version = 'unknown'
+	if (fs.existsSync(VERSION_FILENAME)) {
+		version = fs.readFileSync(VERSION_FILENAME).toString().substr(0, 8)
+	}
+	res
+		.set('x-powered-by', 'cyclic.sh')
+		.json({
+			msg: "Not strickly part of the hello world but you get the picture.",
+			version,
+			at: new Date().toISOString(),
+			method: req.method,
+			hostname: req.hostname,
+			ip: req.ip,
+			path: req.params[0],
+			query: req.query,
+			headers: req.headers,
+			cookies: req.cookies,
+		})
+		.end()
 })
 
 module.exports = app
